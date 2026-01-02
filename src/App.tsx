45902ed4b2d4f3e7fe2de9fcb7e15c2efd3379c3
mohaabdinasir <mohamedabdinasir1414@gmail.com>
2026-01-02 12:03:46 +0300

    Fixed errors route

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
            <Routes>
                <Route path="/" element={<Home />} />
                <Route path="/edit" element={<Edit />} />
-               <Route path="/*" element={<Home />} />
+               <Route path="/*" element={<Home fromUnknowPath={true}/>} />
            </Routes>
        </BrowserRouter>
        </PopupProvider>
